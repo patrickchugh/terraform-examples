fd5e719a1d4d7f8102046aca67061b6e79cce16a
patrickchugh <me@patrickchugh.com>
2023-11-20 11:53:14 +0000

    fix

--- aws/wordpress_fargate/provider.tf
+++ aws/wordpress_fargate/provider.tf
@@ -1,16 +1,16 @@
 provider "aws" {
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "mybucket"
-#     key    = "wordpress"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "wordpress"
+  }
+}
 
 provider "aws" {
-  alias   = "us_east_1"
-  region  = "us-east-1"
+  alias  = "us_east_1"
+  region = "us-east-1"
 }
 
 provider "random" {
