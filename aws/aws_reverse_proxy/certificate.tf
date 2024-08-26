90a31ab463f35d1623d6ea6172da54e5d86477ef
Patrick Chugh <99465091+patrickchugh@users.noreply.github.com>
2024-08-26 15:38:53 +0100

    Update certificate.tf

--- aws/aws_reverse_proxy/certificate.tf
+++ aws/aws_reverse_proxy/certificate.tf
@@ -9,10 +9,10 @@ resource "aws_acm_certificate" "this" {
 
 # Add the DNS records needed by the ACM validation process
 resource "aws_route53_record" "cert_validation" {
-  name    = "${aws_acm_certificate.this.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.this.domain_validation_options.0.resource_record_type}"
+  name    = "${aws_acm_certificate.this.domain_validation_options.resource_record_name}"
+  type    = "${aws_acm_certificate.this.domain_validation_options.resource_record_type}"
   zone_id = "${data.aws_route53_zone.this.zone_id}"
-  records = ["${aws_acm_certificate.this.domain_validation_options.0.resource_record_value}"]
+  records = ["${aws_acm_certificate.this.domain_validation_options.resource_record_value}"]
   ttl     = 60
 }
 
