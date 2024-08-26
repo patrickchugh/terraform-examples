0c5e9de52fb4310707a9d9c78f107aadbaba7183
Patrick Chugh <99465091+patrickchugh@users.noreply.github.com>
2024-08-26 15:51:58 +0100

    Update certificate.tf

--- aws/aws_reverse_proxy/certificate.tf
+++ aws/aws_reverse_proxy/certificate.tf
@@ -16,6 +16,12 @@ resource "aws_route53_record" "cert_validation" {
       type   = dvo.resource_record_type
     }
   }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.this.zone_id
 
  # name    = "${aws_acm_certificate.this.domain_validation_options.resource_record_name}"
   #type    = "${aws_acm_certificate.this.domain_validation_options.resource_record_type}"
