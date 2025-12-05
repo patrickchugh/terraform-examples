2c2a04e2bfecc5fd2027f1ebafaea94c6b7f110b
Patrick Chugh <99465091+patrickchugh@users.noreply.github.com>
2025-12-05 22:32:57 +0000

    Update route53.tf

--- aws/wordpress_fargate/route53.tf
+++ aws/wordpress_fargate/route53.tf
@@ -1,7 +1,7 @@
 # ACM
 data "aws_route53_zone" "this" {
   #name = replace(var.site_domain, "/.*\\b(\\w+\\.\\w+)\\.?$/", "$1") # gets domain from subdomain e.g. "foo.example.com" => "example.com"
-  name = "foo.example.com"
+  name = "example.com"
   #zone_id = "Z1017156O279JAVBAIZ3"
 }
 
